78eb9f86b18c34cc726109f08da023dc864f7d4f
arunajain <aruna.jaindev@gmail.com>
2025-04-22 00:17:30 -0600

    add .env file and database connection to store article data

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 const pool = require('../config/db');
 
 /* GET home page. */
 router.get('/', async (req, res) => {
   try {
+    console.log('heree')
     const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
-
+    console.log('data:', data);
     const articles = data.articles.map(article => ({
       title: article.title,
       description: article.description,
@@ -25,9 +26,14 @@ router.get('/', async (req, res) => {
     const result = await pool.query('SELECT * FROM articles ORDER BY published_at DESC');
     res.render('index', { articles: result.rows });
   } catch (err) {
+    console.log('error showing')
+    console.log(err);
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
 
+// router.get('/',(req, res) => {
+//   res.render('index');
+// });
 module.exports = router;
