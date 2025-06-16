b7d461962c72eab71d88f348e86a48cc04c96ed2
arunajain <aruna.jaindev@gmail.com>
2025-06-16 11:01:05 -0600

    error resovled

--- db/seed.sql
+++ db/seed.sql
@@ -7,7 +7,7 @@ INSERT INTO categories (name) VALUES
   ('Sports'),
   ('Technology');
 
-INSERT INTO users (username, password, email) VALUES
+INSERT INTO users (username, password_hash, email) VALUES
   ('admin', '$2b$10$Wv85qx6dOBcd2UOxvRSX0OcVtMgEDjPH.BUPpIvpnUdIvmXkpQ0gG', 'admin@example.com'),
   ('user1', '$2b$10$BGHEy0eW7g0bUT7UUM5LFO9L1p82zrPH0oHD6SrOay.8TeQ3ct4dC', 'user1@example.com');
 
